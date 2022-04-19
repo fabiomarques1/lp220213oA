875a3f5bf75afb0f5a8d0ccc7789f1d87c7c8805
fabiomarques1 <94563590+fabiomarques1@users.noreply.github.com>
2022-04-19 10:04:19 -0300

    Add files via upload

--- Conta1/src/conta1/Principal.java
+++ Conta1/src/conta1/Principal.java
@@ -1,8 +1,35 @@
 package conta1;
 
+import javax.swing.JOptionPane;
+
 public class Principal {
 
     public static void main(String[] args) {
-        // TODO c
-    }
+        //Declarar Variáveis
+        String teclado;
+        Double quantia;
+        
+        //Realizar Cadastro
+        Conta minhaconta = new Conta();
+        teclado = JOptionPane.showInputDialog("Informe o código do banco:");
+        minhaconta.codigoBanco = Integer.parseInt(teclado);
+        teclado = JOptionPane.showInputDialog("Informe o nome do banco:");
+        minhaconta.nomeBanco = teclado;
+        teclado = JOptionPane.showInputDialog("Informe o número da agência:");
+        minhaconta.numeroAgencia = Integer.parseInt(teclado);
+        teclado = JOptionPane.showInputDialog("Informe o número da conta:");
+        minhaconta.numeroConta = Integer.parseInt(teclado);
+        teclado = JOptionPane.showInputDialog("Informe o cpf do titular:");
+        minhaconta.cpf = Long.parseLong(teclado);
+        teclado = JOptionPane.showInputDialog("Informe o nome do titular:");
+        minhaconta.titular = teclado;
+        
+        //Realizar Depósito
+        teclado = JOptionPane.showInputDialog("Informe o valor do depósito:");
+        quantia = Double.parseDouble(teclado);
+        minhaconta.depositar(quantia);
+        
+        System.out.println("Minha Conta: " + minhaconta);
+}
+    
 }
